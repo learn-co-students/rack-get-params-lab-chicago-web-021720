468d8a6f41d801eff37904e6e1f4c7b3260d22b1
owenabbott <ocabbott@gmail.com>
2020-03-09 07:47:29 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,7 @@
+require 'pry'
 class Application
 
+  @@cart = []
   @@items = ["Apples","Carrots","Pears"]
 
   def call(env)
@@ -11,8 +13,41 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+        search_term = req.params["q"]
+        resp.write handle_search(search_term)
+
+
+
+        
+
+
+
+
+    elsif req.path.match(/cart/) && @@cart.length > 0
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+    
+      end
+    elsif req.path.match(/cart/) && @@cart.length == 0 
+      resp.write "Your cart is empty."
+
+
+    elsif req.path.match(/add/)
+      search_term = req.params
+        search_term.each do |key, value|
+          if @@items.include?(value)
+          @@cart << value 
+          resp.write "added #{value}"
+        else
+          resp.write "We don't have that item"
+        end
+      end
+      #binding.pry
+
+
+
+
+
     else
       resp.write "Path Not Found"
     end
